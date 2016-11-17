51153a5ecb2542d3adb74ef64fd923a42cf63500
Jim Burton <jim@sdf-eu.org>
2016-11-17 17:05:30 +0000

    tidy up

--- src/main/java/CI346/dimensions/Main.java
+++ src/main/java/CI346/dimensions/Main.java
@@ -1,10 +1,11 @@
-package CI346;
+package CI346.dimensions;
 
 import org.jscience.economics.money.Currency;
 import org.jscience.economics.money.Money;
 import org.jscience.physics.amount.Amount;
 
 import javax.measure.quantity.Length;
+import javax.measure.unit.Unit;
 import javax.measure.unit.UnitFormat;
 
 import static javax.measure.unit.NonSI.*;
@@ -18,7 +19,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("");
         System.out.println("Money/Currencies");
         ///////////////////////////////////////////////////////////////////////
         // Calculates the cost of a car trip in Europe for an American tourist.
@@ -34,8 +34,8 @@ public class Main {
         EUR.setExchangeRate(1.07); // 1.0 € = 1.07 $
 
         // Calculates trip cost.
-        Amount<?> carMileage = Amount.valueOf(20, MILE
-                .divide(GALLON_LIQUID_US)); // 20 mi/gal.
+        Unit<?> mpg = MILE.divide(GALLON_LIQUID_US);
+        Amount<?> carMileage = Amount.valueOf(20, mpg); // 20 mi/gal.
         Amount<?> petrolPrice = Amount.valueOf(1.2, EUR.divide(LITRE)); // 1.2 €/L
 
         Amount<Length> tripDistance = Amount.valueOf(400, KILO(METRE)); // 400 km
@@ -45,10 +45,16 @@ public class Main {
         System.out.println("Trip cost = " + tripCost + " ("
                 + tripCost.to(EUR) + ")");
 
+        /*
+        1. Write code using the same methods as above to calculate the cost of a car journey
+        from New York to San Jose. You'll need to check the exchange rate for Sterling, i.e.
+        how many pounds you get for one dollar.
+         */
 
-        //Convert the NonSI unit lbf.s of impulse (pounds of force per second) to
-        // its SI equivalent N.s (Newtons per second)
-
+        /*
+        2. Convert the NonSI unit lbf.s/lb of specific impulse (impulse per pound of fuel) to
+        its SI equivalent N.s/kg. The specific impulse of the Orbiter was 310 lbf.s/lb.
+         */
 
     }
 }
